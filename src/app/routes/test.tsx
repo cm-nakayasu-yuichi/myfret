c73495c90f8a88cde7f0a63d2cb52084985e5c9b
nakayasu yuichi <nakayasu.yuichi@classmethod.jp>
2025-02-13 00:54:09 +0900

    APIのURLを環境変数として追加

--- src/app/routes/test.tsx
+++ src/app/routes/test.tsx
@@ -3,6 +3,8 @@ import { Container } from "@mui/material";
 export const TestRoute = () => {
 
     return (
-        <Container sx={{ my: 4 }} />
+        <Container sx={{ my: 4 }} >
+            {import.meta.env.VITE_API_BASE_URL}
+        </Container>
     );
 };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+/* 環境変数定義 */
+interface ImportMetaEnv {
+    readonly VITE_API_BASE_URL: string;
+}
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
