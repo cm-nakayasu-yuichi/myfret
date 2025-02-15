cad401b6b8aad8a0f6671b1573dacfa0d4dc50b0
nakayasu yuichi <nakayasu.yuichi@classmethod.jp>
2025-02-16 00:31:32 +0900

    SearchArtistsPageを作成

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,5 +1,6 @@
 export * from "./home";
 export * from "./artist";
+export * from "./search-artists";
 export * from "./search-songs";
 export * from "./ranking-artists";
 export * from "./ranking-songs";
