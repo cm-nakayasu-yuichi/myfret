ef430c2cd4e65d7053ff5ecfc62ce1e9f8999ec2
nakayasu yuichi <nakayasu.yuichi@classmethod.jp>
2025-02-16 00:29:02 +0900

    SearchSongsPageの作成

--- src/app/routes/search-songs.tsx
+++ src/app/routes/search-songs.tsx
@@ -1,37 +1,5 @@
-// src/app/routes/SearchSongsRoute.tsx
-import { useParams } from "react-router-dom";
-import { useSearchSongs } from "../../hooks/useSearchSongs";
-import { ListContainer } from "../../components/layouts/list-container";
-import { NormalListItem } from "../../components/common/list-item";
-import { SongListText } from "../../components/common/list-text";
-import { SectionTitle } from "../../components/common/section-title";
-import { PageContainer } from "../../components/layouts/page-container";
-import { NormalList } from "../../components/common/list";
+import { SearchSongsPage } from "../../components/pages";
 
 export const SearchSongsRoute = () => {
-    const { keyword = "" } = useParams<{ keyword: string }>();
-    const { loading, error, result } = useSearchSongs(keyword);
-
-    return (
-        <PageContainer>
-            <SectionTitle total={result?.count}>楽曲ランキング</SectionTitle>
-            <ListContainer
-                empty={(result?.songs.length ?? 0) <= 0}
-                error={error}
-                loading={loading}
-            >
-                <NormalList>
-                    {result?.songs.map((song, index) => (
-                        <NormalListItem
-                            index={index}
-                            key={song.id}
-                            to={`/song/${song.id}`}
-                        >
-                            <SongListText song={song} />
-                        </NormalListItem>
-                    ))}
-                </NormalList>
-            </ListContainer>
-        </PageContainer>
-    );
+    return (<SearchSongsPage />);
 };
--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,4 +1,5 @@
 export * from "./home";
 export * from "./artist";
+export * from "./search-songs";
 export * from "./ranking-artists";
 export * from "./ranking-songs";
