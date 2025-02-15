2c1711e95aa43113b3e347e0cac61f167e616ff7
nakayasu yuichi <nakayasu.yuichi@classmethod.jp>
2025-02-16 05:06:00 +0900

    テストページで動作確認できるようにした

--- src/app/routes/test.tsx
+++ src/app/routes/test.tsx
@@ -1,10 +1,13 @@
 import { Container } from "@mui/material";
+import { transposeChord } from "../../types";
 
 export const TestRoute = () => {
 
     return (
         <Container sx={{ my: 4 }} >
-            {import.meta.env.VITE_API_BASE_URL}
+            {transposeChord("C#", 3)}<br />
+            {transposeChord("B♭", 10)}<br />
+            {transposeChord("A♭sus4/E♭", 0)}<br />
         </Container>
     );
 };
