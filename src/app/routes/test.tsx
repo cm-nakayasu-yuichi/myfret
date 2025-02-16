6855c8a6ae5322da1a8f1fa67815601a3313339f
nakayasu yuichi <nakayasu.yuichi@classmethod.jp>
2025-02-16 23:22:02 +0900

    動作確認のためテストページを改造

--- src/app/routes/test.tsx
+++ src/app/routes/test.tsx
@@ -1,13 +1,51 @@
-import { Container } from "@mui/material";
-import { transposeChord } from "../../types";
+import { Box, Paper, Typography } from "@mui/material";
+import { ChordDiagramViewer } from "../../components/common/chord-diagram";
 
 export const TestRoute = () => {
 
     return (
-        <Container sx={{ my: 4 }} >
-            {transposeChord("C#", 3)}<br />
-            {transposeChord("B♭", 10)}<br />
-            {transposeChord("A♭sus4/E♭", 0)}<br />
-        </Container>
+        <Paper
+            sx={{
+                width: 240,
+                p: 2,
+                borderRadius: 0,
+                borderLeft: 1,
+                borderColor: "divider",
+                bgcolor: "#f5f5f5",
+                display: "flex",
+                flexDirection: "column",
+            }}
+        >
+            <Box sx={{ flex: 1, display: "flex", flexDirection: "column" }}>
+                <Box
+                    sx={{
+                        width: "100%",
+                        paddingBottom: "100%",
+                        position: "relative",
+                        border: 1,
+                        borderColor: "divider",
+                        borderRadius: 1,
+                        mb: 1,
+                    }}
+                >
+                    <Box sx={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                    }}>
+                        <ChordDiagramViewer
+                            chordName='Am'
+                            onNext={() => { }}
+                            onPrevious={() => { }}
+                        />
+                    </Box>
+                </Box>
+                <Typography variant="caption" sx={{ textAlign: "center" }}>
+                    コードをクリックすると、ここに押さえ方が表示されます
+                </Typography>
+            </Box>
+        </Paper>
     );
 };
