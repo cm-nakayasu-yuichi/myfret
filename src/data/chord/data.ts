5b5da69ad036cfa9352bd7fab147a24ce494c79e
nakayasu yuichi <nakayasu.yuichi@classmethod.jp>
2025-02-23 02:39:35 +0900

    コメント追記

--- src/data/chord/data.ts
+++ src/data/chord/data.ts
@@ -6,79 +6,116 @@ import { ChordPosition } from "./interfaces";
  * - [1弦, 2弦, 3弦, 4弦, 5弦, 6弦]
  *   - 0は開放弦を表す
  *   - -1はミュート弦を表す
+ * - barresの定義がある場合(配列要素数は1であることが前提)、派生形のコードを作ることができる
  */
 export const OPEN_POSITIONS: Record<
     string,
     Record<string, ChordPosition | null>
 > = {
-    "major": {
-        "C":  { frets: [0, 1, 0, 2, 3, 0], barres: [{ fret: 0, strings: [1, 5] }] },
+    major: {
+        C: {
+            frets: [0, 1, 0, 2, 3, 0],
+            barres: [{ fret: 0, strings: [1, 5] }],
+        },
         "C#": null,
-        "D":  { frets: [2, 3, 2, 0, -1, -1], barres: [{ fret: 0, strings: [4, 4] }] },
+        D: {
+            frets: [2, 3, 2, 0, -1, -1],
+            barres: [{ fret: 0, strings: [4, 4] }],
+        },
         "D#": null,
-        "E":  { frets: [0, 0, 1, 2, 2, 0], barres: [{ fret: 0, strings: [1, 6] }] },
-        "F":  null,
+        E: {
+            frets: [0, 0, 1, 2, 2, 0],
+            barres: [{ fret: 0, strings: [1, 6] }],
+        },
+        F: null,
         "F#": null,
-        "G":  { frets: [3, 0, 0, 0, 2, 3] },
+        G: { frets: [3, 0, 0, 0, 2, 3] },
         "G#": null,
-        "A":  { frets: [0, 2, 2, 2, 0, -1], barres: [{ fret: 0, strings: [1, 5] }] },
+        A: {
+            frets: [0, 2, 2, 2, 0, -1],
+            barres: [{ fret: 0, strings: [1, 5] }],
+        },
         "A#": null,
-        "B":  null,
+        B: null,
     },
-    "minor": {
-        "C":  null,
+    minor: {
+        C: null,
         "C#": null,
-        "D":  { frets: [1, 3, 2, 0, -1, -1] },
+        D: { frets: [1, 3, 2, 0, -1, -1] },
         "D#": null,
-        "E":  { frets: [0, 0, 0, 2, 2, 0], barres: [{ fret: 0, strings: [1, 6] }] },
-        "F":  null,
+        E: {
+            frets: [0, 0, 0, 2, 2, 0],
+            barres: [{ fret: 0, strings: [1, 6] }],
+        },
+        F: null,
         "F#": null,
-        "G":  null,
+        G: null,
         "G#": null,
-        "A":  { frets: [0, 1, 2, 2, 0, -1], barres: [{ fret: 0, strings: [1, 5] }] },
+        A: {
+            frets: [0, 1, 2, 2, 0, -1],
+            barres: [{ fret: 0, strings: [1, 5] }],
+        },
         "A#": null,
-        "B":  null,
+        B: null,
     },
     "7": {
-        "C":  null,
+        C: null,
         "C#": null,
-        "D":  { frets: [2, 1, 2, 0, -1, -1] },
+        D: { frets: [2, 1, 2, 0, -1, -1] },
         "D#": null,
-        "E":  { frets: [0, 0, 1, 0, 2, 0], barres: [{ fret: 0, strings: [1, 6] }] },
-        "F":  null,
+        E: {
+            frets: [0, 0, 1, 0, 2, 0],
+            barres: [{ fret: 0, strings: [1, 6] }],
+        },
+        F: null,
         "F#": null,
-        "G":  { frets: [1, 0, 0, 0, 2, 3] },
+        G: { frets: [1, 0, 0, 0, 2, 3] },
         "G#": null,
-        "A":  { frets: [0, 2, 0, 2, 0, -1], barres: [{ fret: 0, strings: [1, 5] }] },
+        A: {
+            frets: [0, 2, 0, 2, 0, -1],
+            barres: [{ fret: 0, strings: [1, 5] }],
+        },
         "A#": null,
-        "B":  null,
+        B: null,
     },
-    "m7": {
-        "C":  null,
+    m7: {
+        C: null,
         "C#": null,
-        "D":  { frets: [1, 1, 2, 0, -1, -1] },
+        D: { frets: [1, 1, 2, 0, -1, -1] },
         "D#": null,
-        "E":  { frets: [0, 0, 0, 0, 2, 0], barres: [{ fret: 0, strings: [1, 6] }] },
-        "F":  null,
+        E: {
+            frets: [0, 0, 0, 0, 2, 0],
+            barres: [{ fret: 0, strings: [1, 6] }],
+        },
+        F: null,
         "F#": null,
-        "G":  null,
+        G: null,
         "G#": null,
-        "A":  { frets: [0, 1, 0, 2, 0, -1], barres: [{ fret: 0, strings: [1, 5] }] },
+        A: {
+            frets: [0, 1, 0, 2, 0, -1],
+            barres: [{ fret: 0, strings: [1, 5] }],
+        },
         "A#": null,
-        "B":  null,
+        B: null,
     },
-    "maj7": {
-        "C":  { frets: [0, 0, 0, 2, 3, 0] },
+    maj7: {
+        C: { frets: [0, 0, 0, 2, 3, 0] },
         "C#": null,
-        "D":  null,
+        D: null,
         "D#": null,
-        "E":  { frets: [0, 0, 1, 1, 2, 0], barres: [{ fret: 0, strings: [1, 6] }] },
-        "F":  null,
+        E: {
+            frets: [0, 0, 1, 1, 2, 0],
+            barres: [{ fret: 0, strings: [1, 6] }],
+        },
+        F: null,
         "F#": null,
-        "G":  { frets: [2, 0, 0, 0, 2, 3] },
+        G: { frets: [2, 0, 0, 0, 2, 3] },
         "G#": null,
-        "A":  { frets: [0, 2, 1, 2, 0, -1], barres: [{ fret: 0, strings: [1, 5] }] },
+        A: {
+            frets: [0, 2, 1, 2, 0, -1],
+            barres: [{ fret: 0, strings: [1, 5] }],
+        },
         "A#": null,
-        "B":  null,
+        B: null,
     },
 } as const;
